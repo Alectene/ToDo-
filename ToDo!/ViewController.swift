a8ae17558d9b2a6ba44c3c8e4c4e5788bbb9442d
alectene <alectene@mail.sfsu.edu>
2020-04-22 14:38:36 -0700

    can set photo to image

--- ToDo!/ViewController.swift
+++ ToDo!/ViewController.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
+import AlamofireImage
 
-class ViewController: UIViewController, UITextFieldDelegate {
+class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     //Properties
     
@@ -16,6 +17,9 @@ class ViewController: UIViewController, UITextFieldDelegate {
     
     
     @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,13 +42,56 @@ class ViewController: UIViewController, UITextFieldDelegate {
         TaskNameLabel.text = textField.text
     }//end func textFieldDidEndEditing
     
+    //UIImagePickerControllerDelegate
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        // Dismiss the picker if the user canceled.
+        dismiss(animated: true, completion: nil)
+    }// end func imagePickerControllerDidCancel
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+       
+    // The info dictionary may contain multiple representations of the image. You want to use the original.
+        guard let selectedImage = info[.originalImage] as? UIImage else {
+           fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
+       }
+        
+        // Set photoImageView to display the selected image.
+        photoImageView.image = selectedImage
+        
+        // Dismiss the picker.
+        dismiss(animated: true, completion: nil)
+       
+    
+   }// end func imagePickerController
+    
+    
+    
+    
     //Actions
     
     @IBAction func setDefaultLabelText(_ sender: UIButton) {
         //changes TaskNameLabel to Default Text
         TaskNameLabel.text = "Default Text"
-    }
+    }// end func setDefaultLabelText
     
 
+    @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
+        
+        // Hide the keyboard.
+           nameTextField.resignFirstResponder()
+           
+           // UIImagePickerController is a view controller that lets a user pick media from their photo library.
+           let imagePickerController = UIImagePickerController()
+           
+           // Only allow photos to be picked, not taken.
+           imagePickerController.sourceType = .photoLibrary
+           
+           // Make sure ViewController is notified when the user picks an image.
+           imagePickerController.delegate = self
+           present(imagePickerController, animated: true, completion: nil)
+        
+        
+    }//end func selectImageFromPhotoLibrary
 }
 
